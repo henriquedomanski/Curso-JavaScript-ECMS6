2e1ff3c339c4ec49b92758576d18ea2a45d2bf31
Henrique Domanski <henrydomanski@gmail.com>
2020-01-16 16:03:53 -0300

    Aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log (`A velocidade do seu carro é ${vel}`) 
+    if (vel > 60){ //condição simples
+
+        console.log (`Voce ultrapassou a velocidade permitida. MULTADO!`)
+    }
+console.log (`Didija sempre usando cinto de segurança! `)
