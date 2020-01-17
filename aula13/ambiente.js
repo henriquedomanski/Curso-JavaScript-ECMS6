20d95f4330d71f9e42ee783651df86a480965f67
Henrique Domanski <henrydomanski@gmail.com>
2020-01-17 10:49:20 -0300

    Aula13 - While

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+
+
+
+var c = 1
+do { //Do while: Estrutura de repetição com teste lógico no final
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+/*
+var c = 1
+while (c <= 6){ //While: Estrutura de repetição com teste lógico no inicio
+    console.log(`Passo ${c}`)
+    c++
+}
+
+*/
+
+/*
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+*/
