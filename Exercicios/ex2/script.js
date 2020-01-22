66ea503b364de24cb875ecdac0f914dd465910f3
Henrique Domanski <henrydomanski@gmail.com>
2020-01-22 18:22:33 -0300

    ex 2 final

--- Exercicios/ex2/script.js
+++ Exercicios/ex2/script.js
@@ -4,27 +4,10 @@ function calcular() {
     var n1 = document.getElementById('txtn1').value
     var n2 = document.getElementById('txtn2').value
     var n3 = document.getElementById('txtn3').value
+    var media = parseInt(n1) + parseInt(n2) + parseInt(n3)
 
-    var media1 = n1
-    var media2 = n2
-    var media3 = n3
+    window.alert(`Sua média é: ${media /2}`)
 
-    if (media1 == n1){
-
-        window.alert (`O valor da média 1 é: ${media1 / 2}`)
-
-        if (media2 == n2){
-
-            window.alert (`O valor da media 2 é: ${media2 /2}`)
-
-            if (media3 == n3){
-                window.alert(`O valor da média 3 é: ${media3}`)
-            }
-        }
- 
-    } else {
-
-    }
 
     }
 
