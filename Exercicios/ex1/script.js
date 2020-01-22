a46cfb17c04204b074812dfb57703a1078668a8f
Henrique Domanski <henrydomanski@gmail.com>
2020-01-22 13:26:28 -0300

    ex1

---
+++ Exercicios/ex1/script.js
@@ -0,0 +1,45 @@
+
+
+
+function calcularN1(){
+    var n1 = document.getElementById ('txtN1').value
+    
+    var media = n1
+
+    if (media == n1){
+
+        window.alert (`O valor da média é: ${media / 2}`)
+
+    }
+
+    
+}
+
+
+function calcularN2(){
+    var n2 = document.getElementById ('txtN2').value
+    
+    var media = n2
+
+    if (media == n2){
+
+        window.alert (`O valor da média é: ${media / 2}`)
+
+    }
+
+    
+}
+
+function calcularN3(){
+    var n3 = document.getElementById ('txtN3').value
+    
+    var media = n3
+
+    if (media == n3){
+
+        window.alert (`O valor da média é: ${media / 2}`)
+
+    }
+
+    
+}
