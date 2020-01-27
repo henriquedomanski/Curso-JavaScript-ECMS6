165c99abdc179e6ef1cf39b83f6cba8cf675c931
Henrique Domanski <henrydomanski@gmail.com>
2020-01-27 13:10:03 -0300

    Escopo

---
+++ Avançado/escopo.js
@@ -0,0 +1,32 @@
+// Por qual motivo o código abaixo retorna com erros?
+{//Res: pq a variavel const e let esta sendo chamada fora,
+// de bloco. e a console log está chamando o tipo de varivel (var)
+    var cor = 'preto';
+    const marca = 'Fiat';
+    let portas = 4;
+  }
+  console.log(var, marca, portas);
+  
+  // Como corrigir o erro abaixo?
+  function somarDois(x) {
+    const dois = 2;
+    return x + dois;
+  }
+  function dividirDois(x) {
+    return x / dois;
+  }
+  somarDois(4);
+  dividirDois(6);
+  //Resposta: Declarar a variavel "const dois = 2" fora da função
+  
+  // O que fazer para total retornar 500?
+  //Resposta: Mudar o tipo de varivel dentro do FOR para LET
+  var numero = 50;
+  
+  for(var numero = 0; numero < 10; numero++) {//aqui
+    console.log(numero);
+  }
+  
+  const total = 10 * numero;
+  console.log(total);
+  
