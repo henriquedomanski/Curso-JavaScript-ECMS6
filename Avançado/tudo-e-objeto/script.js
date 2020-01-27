0f0992601bacd67d156fcc25c91733b959cdcac7
Henrique Domanski <henrydomanski@gmail.com>
2020-01-27 11:46:39 -0300

    tudo e objeto

---
+++ Avançado/tudo-e-objeto/script.js
@@ -0,0 +1,25 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Henrique'
+
+// nome.length
+// nome.toUpperCase
+// nome.fixed
+
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector ('.btn')
+// btn.addEventListener
+// btn.appendChild
+// btn.getAttribute
+// btn.innerHTML
+// btn.id
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+// document.execCommand("copy")
+// document.execCommand("cut")
+// document.execCommand("paste")
+
+
