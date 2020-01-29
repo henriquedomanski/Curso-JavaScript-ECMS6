c8da9de9dbebe40c8a96ae7f81f8e3ce30656329
Henrique Domanski <henrydomanski@gmail.com>
2020-01-29 17:44:32 -0300

    Dimensoes e distancias

---
+++ Avançado/DOM/DimensoesEDistancias/script.js
@@ -0,0 +1,56 @@
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+const imagem = document.querySelector('img')
+const imgTop = imagem.offsetTop
+
+
+console.log(imgTop)
+
+
+// Retorne a soma da largura de todas as imagens
+
+function somaImagens(){
+const todasImg = document.querySelectorAll ('img')
+let soma = 0
+todasImg.forEach ((img)=>{
+  soma = soma + img.offsetWidth
+});
+console.log(soma);
+
+}
+window.onload = function(){
+  somaImagens()
+
+}
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+const links = document.querySelectorAll ('a')
+
+links.forEach((link) => {
+  const linkWidth = link.offsetWidth
+  const linkHeight = link.offsetHeight
+
+  if (linkWidth >= 48 && linkHeight >= 48){
+    console.log(link, 'Possui o minimo recomendado')
+
+  } else {
+    console.log(link, 'Nao possui')
+  }
+
+})
+
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+
+const small = window.matchMedia ('max-width: 720px').matches;
+
+if (small){
+  const menu = document.querySelector('.menu')
+  menu.classList.add('menu-mobile')
+
+} else {
+  console.log('é maior')
+}
+
