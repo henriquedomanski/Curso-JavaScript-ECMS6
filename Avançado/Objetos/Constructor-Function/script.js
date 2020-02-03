2d5c848f6ae537cd32362df94f6dde678c52b069
Henrique Domanski <henrydomanski@gmail.com>
2020-02-03 14:54:13 -0300

    constructor function exercicios

--- Avançado/Objetos/Constructor-Function/script.js
+++ Avançado/Objetos/Constructor-Function/script.js
@@ -1,15 +1,18 @@
 // Transforme o objeto abaixo em uma Constructor Function
-const pessoa = {
-    nome: 'Nome pessoa',
-    idade: 0,
-    andar() {
+
+function Pessoa(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    this.andar = function() {
       console.log(this.nome + ' andou');
     }
   }
   
   // Crie 3 pessoas, João - 20 anos,
   // Maria - 25 anos, Bruno - 15 anos
-  
+  const joao = new Pessoa('joao', 20)
+  const maria = new Pessoa ('Maria', 25)
+  const bruno = new Pessoa ('Bruno', 15)
   
   // Crie uma Constructor Function (Dom) para manipulação
   // de listas de elementos do dom. Deve conter as seguintes
@@ -18,5 +21,31 @@ const pessoa = {
   // elements, retorna NodeList com os elementos selecionados
   // addClass(classe), adiciona a classe a todos os elementos
   // removeClass(classe), remove a classe a todos os elementos
+
+  function Dom (seletor){
+    const elementList = document.querySelectorAll (seletor)//estou criando uma variavel em uma função, aonde eu mesmo defino qual elemento do documento irei selecionar (ex: li ou ul)
+    this.element = elementList;
+    this.addClass = function (classe){
+      elementList.forEach ((element)=>{
+        element.classList.add (classe)
+      })
+
+    }
+
+    this.removeClass = function (classe){
+      elementList.forEach((element)=>{
+        element.classList.removeClass(classe)
+      })
+    }
+    
+    
+
+  }
+
+
+  const listaItens = new Dom ('li')
+
+
+
   
   
